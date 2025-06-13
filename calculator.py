df5c535bdcd10898f5885549575560e14a749326
Arun Kumar <java.kumar.arun@gmail.com>
2025-06-13 17:52:26 +0530

    python basics

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# basic calculator
+num1 = 3
+num2 = 4
+
+# arithmatic operations
+print("Sum of ",num1," and ",num2, " is ", num1+num2)
+print(f"Multiplication of {num1} and {num2} is {num1*num2}")
+print(f"Division of {num1} and {num2} is {num1/num2}")
+print(f"Remainder of {num1} and {num2} is {num1%num2}")
+print(f"Quotient of {num1} and {num2} is {num1//num2}")
+print(f"{num1} power {num2} is {num1**num2}")
---
+++ restaurant_bill.py
@@ -0,0 +1,18 @@
+'''
+Sohan's Restaurant, show menu
+    Dosa - 80
+    Idli - 35
+    ColdDrink - 60
+
+Mohan -> 2 Dosa, 5 Idli, 1 ColdDrink 
+find total bill
+'''
+print("Welcome to Sohan's Restaurant")
+print("Our menu")
+print("1. Dosa - Rs 80")
+print("2. Idli - Rs 35")
+print("3. ColdDrink - Rs 60")
+
+billAmount = (2*80)+(5*35)+(1*60)
+
+print("Mohan's total Bill", billAmount)
