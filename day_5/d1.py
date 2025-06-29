9e2119e5fb670d8ef62772f9445b039a2f5710c5
Arun Kumar <java.kumar.arun@gmail.com>
2025-06-29 10:32:02 +0530

    python patterns

---
+++ day_5/d1.py
@@ -0,0 +1,33 @@
+# 1. Bitwise AND (&)
+a = 5  # 0101 in binary
+b = 3  # 0011 in binary
+result = a & b  # 0001 in binary (1 in decimal)
+print(result)  # Output: 1
+# 2. Bitwise OR (|)
+
+a = 5  # 0101 in binary
+b = 3  # 0011 in binary
+result = a | b  # 0111 in binary (7 in decimal)
+print(result)  # Output: 7
+# 3. Bitwise XOR (^)
+
+a = 5  # 0101 in binary
+b = 3  # 0011 in binary
+result = a ^ b  # 0110 in binary (6 in decimal)
+print(result)  # Output: 6
+# 4. Bitwise NOT (~)
+
+a = 5  # 0101 in binary
+result = ~a  # 1010 in binary (-6 in decimal)
+print(result)  # Output: -6
+
+# 5. Left Shift (<<)
+a = 5  # 0101 in binary
+result = a << 2  # 10100 in binary (20 in decimal)
+print(result)  # Output: 20
+# 6. Right Shift (>>)
+
+a = 5  # 0101 in binary
+result = a >> 2  # 0001 in binary (1 in decimal)
+print(result)  # Output: 1
+
---
+++ day_5/demo.py
@@ -0,0 +1,4 @@
+a = 6  # 0101 in binary
+b = 3  # 0011 in binary
+result = a | b  # 0111 in binary (7 in decimal)
+print(result)  # Output: 7
